b8ead7a4b42d3b6980f2343733bf64d10644a3c0
boyboi86 <boyboi86@gmail.com>
2016-08-18 15:45:41 +0800

    added socket.io

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var http = require('http').Server(app);
 
 var port = process.env.PORT || 3000,
 	app = express();
 
-app.user(express.static(__dirname + '/public'));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(){
+	console.log('user connected')
+});
 
 http.listen(port, function(){
 	console.log('server listening on: ' + port)
