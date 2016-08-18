02b23e4b3fe1a3cfe0e723773edab3719a358b14
boyboi86 <boyboi86@gmail.com>
2016-08-18 16:46:25 +0800

    added custom chat event

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
+io.on('connection', function(socket){
 	console.log('user connected')
+	socket.on('message', function (message){
+		console.log('Message received' + message.text);
+		//send to everyone including sender: io.emit()
+		//send to everyone excluding sender
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to chat application!'
+	})
 });
 
 http.listen(port, function(){
