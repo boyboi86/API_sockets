4aa3bc99a21579af46f358addb03f0da8cf0209b
boyboi86 <boyboi86@gmail.com>
2016-08-20 18:57:38 +0800

    added moments/timestamp

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 
 var port = process.env.PORT || 3000,
 	app = express();
@@ -10,15 +11,18 @@ app.use(express.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
 	console.log('user connected')
+
 	socket.on('message', function (message){
-		console.log('Message received' + message.text);
+		console.log('Message received: ' + message.text);
 		//send to everyone excluding sender
 		//socket.broadcast.emit('message', message);
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to chat application!'
+		text: 'Welcome to chat application!',
+		timestamp: moment().valueOf()
 	})
 });
 
