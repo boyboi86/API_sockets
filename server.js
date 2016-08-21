9f56582b7ab858ac6437d93433fe6ab693824580
boyboi86 <boyboi86@gmail.com>
2016-08-21 14:01:01 +0800

    added individual chatroom

--- server.js
+++ server.js
@@ -9,15 +9,28 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+//memory cache
+var clientInfo = {};
+
 io.on('connection', function(socket){
 	console.log('user connected')
+
+	socket.on('joinRoom', function (req) {
+		clientInfo[socket.id] = req
+		socket.join(req.room);
+		socket.broadcast.to(req.room).emit('message', {
+			name: 'System',
+			text: req.name + ' has joined!',
+			timestamp: moment().valueOf()
+		})
+	});
 
 	socket.on('message', function (message){
 		console.log('Message received: ' + message.text);
 		//send to everyone excluding sender
 		//socket.broadcast.emit('message', message);
 		message.timestamp = moment().valueOf();
-		io.emit('message', message);
+		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
 
 	socket.emit('message', {
