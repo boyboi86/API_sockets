2bc6aa75e530a80bdd405a5224ec5f11fbf09f51
boyboi86 <boyboi86@gmail.com>
2016-08-22 13:08:38 +0800

    included message on disconnect event

--- server.js
+++ server.js
@@ -11,10 +11,24 @@ app.use(express.static(__dirname + '/public'));
 
 //memory cache
 var clientInfo = {};
-
+//event on 'connection'
 io.on('connection', function(socket){
 	console.log('user connected')
+//event on 'disconnect' leave room msg emit
+	socket.on('disconnect', function (){
+		var userData = clientInfo[socket.id];
+		if(typeof userData !== 'undefined'){
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + ' has left',
+				timestamp: moment().valueOf()
+			});
+			delete clientInfo[socket.id];
+		}
+	});
 
+//broadcast is for everyone
 	socket.on('joinRoom', function (req) {
 		clientInfo[socket.id] = req
 		socket.join(req.room);
@@ -24,7 +38,7 @@ io.on('connection', function(socket){
 			timestamp: moment().valueOf()
 		})
 	});
-
+//io.to is specific
 	socket.on('message', function (message){
 		console.log('Message received: ' + message.text);
 		//send to everyone excluding sender
@@ -32,7 +46,7 @@ io.on('connection', function(socket){
 		message.timestamp = moment().valueOf();
 		io.to(clientInfo[socket.id].room).emit('message', message);
 	});
-
+//socket.emit is for everyone
 	socket.emit('message', {
 		name: 'System login on',
 		text: 'Welcome to chat application!',
